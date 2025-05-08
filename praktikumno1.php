45bb197ccd25715443bcf06fecaea5836c7a3a8f
CharlesPutra <charlesagustianputra@gmail.com>
2025-05-08 11:04:59 +0700

    Add files via upload

---
+++ praktikumno1.php
@@ -0,0 +1,29 @@
+<?php
+
+//isi variabel dengan case yang ada
+$bulan = 4;
+
+switch ($bulan) {
+    case 1:
+        echo "bulan january";
+        break;
+    case 2:
+        echo "bulan february";
+        break;
+    
+    case 3:
+        echo "bulan maret";
+        break;
+    
+    case 4:
+        echo "bulan april";
+        break;
+    
+    case 5:
+        echo "bulan mei";
+        break;
+    default:
+    echo "bulan tidak ditemukan";
+    break;
+}
+?>
